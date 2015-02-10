0cd122d088a40967c826d7d5e8f2d4b007a26369
Robinson Mann <RobinsonMann@users.noreply.github.com>
2015-02-09 23:03:35 -0500

    Refactoring

--- UWaterlooAPI.cs
+++ UWaterlooAPI.cs
@@ -1,6 +1,6 @@
-using UWaterlooAPI.FoodServices;
+using UWaterlooApi.FoodServices;
 
-namespace UWaterlooAPI
+namespace UWaterlooApi
 {
 	/// <summary>
 	/// UWaterlooAPI Class
@@ -10,35 +10,6 @@ namespace UWaterlooAPI
 		// Your API key
 		readonly string _apiKey;
 
-		/// <summary>
-		/// Meta information from each function call
-		/// "method" not implemented
-		/// </summary>
-		public class Meta
-		{
-			public int Requests, Timestamp, Status, MethodId, Version;
-			public string Message;
-		}
-
-		/// <summary>
-		/// An API wrapper. Contains two classes, the first is the API calls meta information, 
-		/// and the second is the 'data' class that the API call returned.
-		/// </summary>
-		/// <typeparam name="T">The type of data that the API call returns</typeparam>
-		public class ApiCall<T> where T : new() {
-			public Meta	Meta;
-			public T	Data;
-
-			/// <summary>
-			/// Default Constructor
-			/// </summary>
-			public ApiCall () {
-				Meta = new Meta();
-				Data = new T();
-			}
-		}
-				
-		
 		public readonly FoodServicesApiService Foodservices;
 		
 		/// <summary>
