55993b2ab8dd21615e649161be8d181932cd564f
Robinson Mann <RobinsonMann@users.noreply.github.com>
2015-02-19 23:41:44 -0500

    Created additional classes.

--- DefinitionsAndCodes/DefinitionsAndCodes.cs
+++ DefinitionsAndCodes/DefinitionsAndCodes.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Newtonsoft.Json;
 using UWaterlooApi.ApiRequest;
 
@@ -21,6 +20,57 @@ namespace UWaterlooAPI.DefinitionsAndCodes
 		/// <summary> "Units full name" (Offical Documentation) </summary>
 		[JsonProperty("unit_full_name")]
 		public string UnitFullName { get; set; }
+	}
+
+	/// <summary>
+	/// WARNING: Offical Documentation IS INCORRECT!
+	/// </summary>
+	public class Term
+	{
+		/// <summary> todo </summary>
+		public string Abbreviation { get; set; }
+		/// <summary> todo </summary>
+		public string Description { get; set; }
+	}
+
+	/// <summary>
+	/// todo
+	/// </summary>
+	public class Group
+	{
+		/// <summary> "Group Code" (Offical Documentation) </summary>
+		[JsonProperty("group_code")]
+		public string GroupCode { get; set; }
+		/// <summary> "Group Short Name" (Offical Documentation) </summary>
+		[JsonProperty("group_short_name")]
+		public string GroupShortName { get; set; }
+		/// <summary> "Full group name" (Offical Documentation) </summary>
+		[JsonProperty("group_full_name")]
+		public string GroupFullName { get; set; }
+	}
+
+	/// <summary>
+	/// todo
+	/// </summary>
+	public class Subject
+	{
+		/// <summary> "Subject" (Offical Documentation) </summary>
+		public string SubjectTitle { get; set; }
+		/// <summary> "Description of subject" (Offical Documentation) </summary>
+		public string Description { get; set; }
+		/// <summary> "Subjects parent unit" (Offical Documentation) </summary>
+		public string Unit { get; set; }
+	}
+
+	/// <summary>
+	/// todo
+	/// </summary>
+	public class Instructions
+	{
+		/// <summary> "Abbreviation" (Offical Documentation) </summary>
+		public string Abbreviation { get; set; }
+		/// <summary> "Description" (Offical Documentation) </summary>
+		public string Description { get; set; }
 	}
 
 	/// <summary>
