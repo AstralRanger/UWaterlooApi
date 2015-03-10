b9c40171084ddb3fa62cc276676148672a520237
Robinson Mann <RobinsonMann@users.noreply.github.com>
2015-03-09 22:17:31 -0400

    Update Weather.cs

--- Weather/Weather.cs
+++ Weather/Weather.cs
@@ -1,76 +1,93 @@
-using Newtonsoft.Json;
-using UWaterlooApi.ApiRequest;
+/* By Robinson Mann */
 
-namespace UWaterlooAPI.Weather
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace UWaterlooApi.Weather
 {
 
-	/// <summary> Information about the current weather in Waterloo </summary>
+	/// <summary> "Current Weather" (Official Documentation) </summary> 
 	public class CurrentWeather
-	{		
-		/// <summary> "Station's latitude" (Offical Documentation) </summary>
-		[JsonProperty("latitude")]
-		public double Latitude;
-		/// <summary> "Station's longitude" (Offical Documentation) </summary>
-		[JsonProperty("longitude")]
-		public double Longitude;
-		/// <summary> "Station elevation in meters" (Offical Documentation) </summary>
+	{
+		/// <summary> "Station's latitude" (Official Documentation) </summary> 
+		public double Latitude { get; set; }
+
+		/// <summary> "Station's longitude" (Official Documentation) </summary> 
+		public double Longitude { get; set; }
+
+		/// <summary> "Station elevation in meters" (Official Documentation) </summary> 
 		[JsonProperty("elevation_m")]
-		public double ElevationM;
-		/// <summary> "iso8601 timestamp of weather recordings" (Offical Documentation) </summary>
+		public double ElevationM { get; set; }
+
+		/// <summary> "iso8601 timestamp of weather recordings" (Official Documentation) </summary> 
 		[JsonProperty("observation_time")]
-		public string ObservationTime;
-		/// <summary> "Current temperature in celsius" (Offical Documentation) </summary>
+		public string ObservationTime { get; set; }
+
+		/// <summary> "Current temperature in celsius" (Official Documentation) </summary> 
 		[JsonProperty("temperature_current_c")]
-		public double TemperatureCurrentC;
-		/// <summary> "Humidex temperature in celsius" (Offical Documentation) </summary>
+		public double TemperatureCurrentC { get; set; }
+
+		/// <summary> "Humidex temperature in celsius" (Official Documentation) </summary> 
 		[JsonProperty("humidex_c")]
-		public double HumidexC;
-		/// <summary> "Windchill in celsius" (Offical Documentation) </summary>
+		public double HumidexC { get; set; }
+
+		/// <summary> "Windchill in celsius" (Official Documentation) </summary> 
 		[JsonProperty("windchill_c")]
-		public double WindchillC;
-		/// <summary> "24 hour maximum temperature in celsius" (Offical Documentation) </summary>
+		public double WindchillC { get; set; }
+
+		/// <summary> "24 hour maximum temperature in celsius" (Official Documentation) </summary> 
 		[JsonProperty("temperature_24hr_max_c")]
-		public double Temperature24HrMaxC;
-		/// <summary> "24 hour minimum temperature in celsius" (Offical Documentation) </summary>
+		public double Temperature24HrMaxC { get; set; }
+
+		/// <summary> "24 hour minimum temperature in celsius" (Official Documentation) </summary> 
 		[JsonProperty("temperature_24hr_min_c")]
-		public double Temperature24HrMinC;
-		/// <summary> "Precipitation reading for 15 minute interval in mm" (Offical Documentation) </summary>
+		public double Temperature24HrMinC { get; set; }
+
+		/// <summary> "Precipitation reading for 15 minute interval in mm" (Official Documentation) </summary> 
 		[JsonProperty("precipitation_15min_mm")]
-		public double Precipitation15MinMm;
-		/// <summary> "Precipitation reading for 1 hour interval in mm" (Offical Documentation) </summary>
+		public double Precipitation15MinMm { get; set; }
+
+		/// <summary> "Precipitation reading for 1 hour interval in mm" (Official Documentation) </summary> 
 		[JsonProperty("precipitation_1hr_mm")]
-		public double Precipitation1HrMm;
-		/// <summary> "Precipitation reading for every 24 hour interval in mm" (Offical Documentation) </summary>
+		public double Precipitation1HrMm { get; set; }
+
+		/// <summary> "Precipitation reading for every 24 hour interval in mm" (Official Documentation) </summary> 
 		[JsonProperty("precipitation_24hr_mm")]
-		public double Precipitation24HrMm;
-		/// <summary> "Relative humidity in percentage" (Offical Documentation) </summary>
+		public double Precipitation24HrMm { get; set; }
+
+		/// <summary> "Relative humidity in percentage" (Official Documentation) </summary> 
 		[JsonProperty("relative_humidity_percent")]
-		public double RelativeHumidityPercent;
-		/// <summary> "Dew point in celsius" (Offical Documentation) </summary>
+		public double RelativeHumidityPercent { get; set; }
+
+		/// <summary> "Dew point in celsius" (Official Documentation) </summary> 
 		[JsonProperty("dew_point_c")]
-		public double DewPointC;
-		/// <summary> "Wind speed in km per hour" (Offical Documentation) </summary>
+		public double DewPointC { get; set; }
+
+		/// <summary> "Wind speed in km per hour" (Official Documentation) </summary> 
 		[JsonProperty("wind_speed_kph")]
-		public double WindSpeedKph;
-		/// <summary> "Wind direction in degrees" (Offical Documentation) </summary>
+		public double WindSpeedKph { get; set; }
+
+		/// <summary> "Wind direction in degrees" (Official Documentation) </summary> 
 		[JsonProperty("wind_direction_degrees")]
-		public double WindDirectionDegrees;
-		/// <summary> "Pressure in kilopascals" (Offical Documentation) </summary>
+		public double WindDirectionDegrees { get; set; }
+
+		/// <summary> "Pressure in kilo Pascals" (Official Documentation) </summary> 
 		[JsonProperty("pressure_kpa")]
-		public double PressureKpa;
-		/// <summary> "Word description of the current pressure trend" (Offical Documentation) </summary>
+		public double PressureKpa { get; set; }
+
+		/// <summary> "Word description of the current pressure trend" (Official Documentation) </summary> 
 		[JsonProperty("pressure_trend")]
-		public string PressureTrend;
-		/// <summary> "Incoming radiation in watts per meter square" (Offical Documentation) </summary>
+		public string PressureTrend { get; set; }
+
+		/// <summary> "Incomig radiation in watts per meter square" (Official Documentation) </summary> 
 		[JsonProperty("incoming_shortwave_radiation_wm2")]
-		public double IncomingShortwaveRadiationWm2;
+		public double IncomingShortwaveRadiationWm2 { get; set; }
 	}
 
-	/// <summary>
-	/// Weather API
-	/// </summary>
+	/// <summary> Weather Endpoints </summary> 
 	public class WeatherApi
 	{
+
 		// The Users University of Waterloo Open Data API Key
 		private readonly string _apiKey;
 
@@ -82,12 +99,15 @@ namespace UWaterlooAPI.Weather
 		}
 
 		/// <summary>
-		/// "This method returns the current weather details from the University of Waterloo Weather Station. http://weather.uwaterloo.ca" (Offical Documentation)
+		/// Official Method Name: Current Weather
+		/// Description: This method returns the current weather details from the University of Waterloo Weather Station. http://weather.uwaterloo.ca
+		/// Update Frequency: Every 15 minutes
+		/// All the above information is from the Official Documentation
 		/// </summary>
-		/// <returns></returns>
-		public ApiRequest<CurrentWeather> CurrentWeather()
+		public ApiRequest<List<CurrentWeather>> CurrentWeather()
 		{
-			return ApiRequest<CurrentWeather>.CreateApiRequest("/weather/current", _apiKey);
+			return ApiRequest<List<CurrentWeather>>.CreateApiRequest("/weather/current", _apiKey);
 		}
+
 	}
 }
